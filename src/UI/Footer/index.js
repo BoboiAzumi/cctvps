2bb8a15e7a7c39638df0c88d4e34abe9b070c735
boboiazumi <untukherokudeploy@gmail.com>
2023-03-13 21:01:22 +0700

    Updates

--- src/UI/Footer/index.js
+++ src/UI/Footer/index.js
@@ -6,13 +6,13 @@ class Footer extends React.Component{
             <>
                 <div className="bgbrown w-full h-fit bottom-0">
                     <div className="py-4">
-                        <div className="flex justify-center py-9">
+                        <div className="flex justify-center py-1.5">
                             <div className='grid grid-flow-row grid-cols-1'>
-                                <div className="flex justify-center py-9">
-                                    <img className='text-center' src="/img/github-mark-white.svg" />
+                                <div className="flex justify-center">
+                                    <img width="20%" src="/img/github-mark-white.svg" />
                                 </div>
-                                <p className='text-center text-white'>BoboiAzumi @ Github</p>
-                                <a className='text-center text-white' href="https://github.com/BoboiAzumi/cctvps">CCTVPS</a>
+                                <p className='text-center text-white mt-2'>BoboiAzumi @ Github</p>
+                                <a className='text-center text-white mb-12' href="https://github.com/BoboiAzumi/cctvps">CCTVPS</a>
                             </div>
                         </div>
                     </div>
